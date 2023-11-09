2fe8f3dd6fa808ac6a05640bd2af5029c3682530
Akash Munagala <akash.munagala@gmail.com>
2023-11-08 21:27:12 -0600

    Draw triangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,72 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+GLuint createVAO() {
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    // Create VAO
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Create and set data to VBO
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Configure vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    return VAO;
+}
+
+GLuint createShaderProgram() {
+    // Shader Source
+    const char *vertexShaderSrc = R"delim(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main() {
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)delim";
+    const char *fragmentShaderSrc = R"delim(
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)delim";
+
+    // Create, set source, and compile shaders
+    GLuint vertexShader, fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
+    // Exercise for the reader: Error handling
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+
+    // Link shader program consisting of vertex and fragment shader
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // After linking, delete shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
 
 int main() {
     glfwInit();
@@ -32,9 +98,19 @@ int main() {
         glViewport(0, 0, width, height);
     });
 
+    GLuint VAO = createVAO();
+    GLuint shaderProgram = createShaderProgram();
+
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.53, 0.81, 0.92, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Bind VAO
+        glBindVertexArray(VAO);
+        // Use Shader
+        glUseProgram(shaderProgram);
+        // Draw
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
