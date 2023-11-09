69a73f3dd81934543709d5f8751d7bf81884d6f4
Akash Munagala <akash.munagala@gmail.com>
2023-11-08 21:37:25 -0600

    Draw rectangle

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 
 GLuint createVAO() {
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
+            0.5f, 0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f, 0.5f, 0.0f   // top left
+    };
+
+    GLuint indices[] = {
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
     };
 
     // Create VAO
@@ -20,6 +26,12 @@ GLuint createVAO() {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create and set data to EBO
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Configure vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
@@ -110,7 +122,7 @@ int main() {
         // Use Shader
         glUseProgram(shaderProgram);
         // Draw
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
